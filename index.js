c7c01efc09fedfbcf26b4b04c9e21f1b904009d2
Michael Few <58129705+holdthez@users.noreply.github.com>
2020-07-31 14:06:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,11 @@ var src = (ele.getAttribute('src') == "michael.png")?"michaelfilter.png":"michae
 ele.setAttribute('src',src);
 })
 
-var elems = document.getElementsByClassName('confirmation1');
+var elems = document.getElementsByClassName('confirmation');
     var confirmIt = function (e) {
-        if (!confirm('Are you sure you want to go to LinkedIn?')) e.preventDefault();
+        if (!confirm('Are you sure you want to leave fewproblems.com?')) e.preventDefault();
     };
     for (var i = 0, l = elems.length; i < l; i++) {
         elems[i].addEventListener('click', confirmIt, false);
     }
-    var elems = document.getElementsByClassName('confirmation2');
-var confirmIt = function (e) {
-        if (!confirm('Are you sure you want to go to Instagram?')) e.preventDefault();
-    };
-    for (var i = 0, l = elems.length; i < l; i++) {
-        elems[i].addEventListener('click', confirmIt, false);
-    }
-var elems = document.getElementsByClassName('confirmation3');
-    var confirmIt = function (e) {
-        if (!confirm('Are you sure? you want to go to Soundcloud')) e.preventDefault();
-    };
-    for (var i = 0, l = elems.length; i < l; i++) {
-        elems[i].addEventListener('click', confirmIt, false);
-    }
+    var elems = document.getElementsByClassName('confirmation');
