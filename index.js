901fdcbd2355105c782e2eedf536f7fcbb6f5530
Michael Few <58129705+holdthez@users.noreply.github.com>
2020-07-30 23:18:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var ele = document.getElementById("image1");
+ele.addEventListener('click',function(){
+var src = (ele.getAttribute('src') == "michael.png")?"michaelfilter.png":"michael.png";
+ele.setAttribute('src',src);
+})
+
 var elems = document.getElementsByClassName('confirmation1');
     var confirmIt = function (e) {
         if (!confirm('Are you sure you want to go to LinkedIn?')) e.preventDefault();
