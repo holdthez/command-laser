7ac55ec5fb4a7abbddc557fe820efaf7bef0af46
Michael Few <58129705+holdthez@users.noreply.github.com>
2020-07-30 18:27:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("Thanks for stopping by!");
+var elems = document.getElementsByClassName('confirmation1');
+    var confirmIt = function (e) {
+        if (!confirm('Are you sure you want to go to LinkedIn?')) e.preventDefault();
+    };
+    for (var i = 0, l = elems.length; i < l; i++) {
+        elems[i].addEventListener('click', confirmIt, false);
+    }
+    var elems = document.getElementsByClassName('confirmation2');
+var confirmIt = function (e) {
+        if (!confirm('Are you sure you want to go to Instagram?')) e.preventDefault();
+    };
+    for (var i = 0, l = elems.length; i < l; i++) {
+        elems[i].addEventListener('click', confirmIt, false);
+    }
+var elems = document.getElementsByClassName('confirmation3');
+    var confirmIt = function (e) {
+        if (!confirm('Are you sure? you want to go to Soundcloud')) e.preventDefault();
+    };
+    for (var i = 0, l = elems.length; i < l; i++) {
+        elems[i].addEventListener('click', confirmIt, false);
+    }
